ede1cbcd76e5ca3fe4713b61b305cd228c81a6c9
Mike <super.mvk@yandex.ru>
2024-06-19 10:08:44 +0000

    update script

--- Arduino-STM32/teleplot-pipe.py
+++ Arduino-STM32/teleplot-pipe.py
@@ -1,7 +1,8 @@
-#!/bin/python
+#!/bin/python3
 import socket
 import math
 import time
+from pathlib import Path
 
 teleplotAddr = ("127.0.0.1",47269)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,12 +12,19 @@ def sendTelemetry(name, value):
     msg = name+":"+str(now)+":"+str(value)+"|g"
     sock.sendto(msg.encode(), teleplotAddr)
 
-
-with open('/tmp/renode-usart2', 'r') as fin:
-    for line in fin:
-        # print('recieved:', repr(line))
-        channel_name, value = line.strip().split(':')
-        sendTelemetry(channel_name, value)
+retry_count = 0
+while retry_count < 3:
+    try:
+        with open('/tmp/renode-usart2', 'r') as fin:
+            for line in fin:
+                # print('recieved:', repr(line))
+                channel_name, value = line.strip().split(':')
+                sendTelemetry(channel_name, value)
+        break
+    except FileNotFoundError:
+        retry_count += 1
+        print('File not found, retrying...')
+        time.sleep(5)
 
 # i=0
 # while i < 1000:
